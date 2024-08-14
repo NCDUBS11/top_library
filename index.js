e0584005297bb3a651fe7d6db830d9fa1d9cadee
nick <112791837+NCDUBS11@users.noreply.github.com>
2024-08-14 05:43:07 -0700

    Restart project

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
 
-const bookList = document.getElementById("bookList");
 
-let listItems;
+
+
+
 
 const myLibrary = [];
 
 
+// function Book(title, surname, pages, readStatus) {
+//     this.title = title;
+//     this.surname = surname;
+//     this.pages = pages;
+//     this.readStatus = readStatus;
+// }
 
-function Book(title, surname, pages, readStatus) {
-    this.title = title;
-    this.surname = surname;
-    this.pages = pages;
-    this.readStatus = readStatus;
-}
+// function addBookToLibrary() {
 
-function addBookToLibrary() {
-    const form = document.getElementById("form");
-    const title = document.getElementById("title").value;
-    const surname = document.getElementById("surname").value;
-    const pages = document.getElementById("pages").value;
-    const readStatus = document.getElementById("read").checked;
+//     title = title.value.trim();
+//     surname = surname.value.trim();
+//     pages = pages.value.trim();
+//     readStatus = readStatus.checked;
+
+//     const li = document.createElement("li");
+//     li.setAttribute("id", `${surname}`);
+//     li.setAttribute("class", `${title}`);
+//     li.innerText = title;
     
-    const li = document.createElement("li");
-    li.setAttribute("id", `${surname}`);
-    li.setAttribute("class", `${title}`);
-    li.innerHTML = title;
-    
-    const book = new Book(title, surname, pages, readStatus);
-    myLibrary.push(book);
-    bookList.appendChild(li);
-    form.reset();
+//     const book = new Book(title, surname, pages, readStatus);
+//     myLibrary.push(book);
+//     bookList.appendChild(li);
+//     form.reset();
+//     listItems = document.querySelectorAll("li");
+//     listItems.forEach((item) => {
+//         item.setAttribute("onclick", "dispInfo()")
+//     });
+// }
 
-    listItems = document.querySelectorAll("li");
-
-    listItems.forEach((item) => {
-        item.setAttribute("onclick", "dispInfo()")
-    });
-}
-
-function dispInfo() {
-    alert("It works!");
-}
+// function dispInfo() {
+//     alert("It works!");
+// }
