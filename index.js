3b757fc856e2afd49fd00da6bfe642b73eabc634
nick <112791837+NCDUBS11@users.noreply.github.com>
2024-08-19 06:28:03 -0700

    Work on JS form validation and functioning

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-
-
-
-
-
+let form = document.getElementById("form");
+// const submitBtn = document.getElementById(submitBtn);
+let title = document.getElementById("bookTitle");
+let surname = document.getElementById("surname");
+let pages = document.getElementById("pages");
+let readStatus = document.getElementById("readStatus");
 
 const myLibrary = [];
 
 
-// function Book(title, surname, pages, readStatus) {
-//     this.title = title;
-//     this.surname = surname;
-//     this.pages = pages;
-//     this.readStatus = readStatus;
-// }
+function Book(title, surname, pages, readStatus) {
+    this.title = title;
+    this.surname = surname;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
 
-// function addBookToLibrary() {
+function addBookToLibrary() {
 
-//     title = title.value.trim();
-//     surname = surname.value.trim();
-//     pages = pages.value.trim();
-//     readStatus = readStatus.checked;
+    title = title.value.trim();
+    surname = surname.value.trim();
+    pages = pages.value.trim();
+    readStatus = readStatus.checked;
 
-//     const li = document.createElement("li");
-//     li.setAttribute("id", `${surname}`);
-//     li.setAttribute("class", `${title}`);
-//     li.innerText = title;
+    const li = document.createElement("li");
+    li.setAttribute("id", `${surname}`);
+    li.setAttribute("class", `${title}`);
+    li.innerText = title;
     
-//     const book = new Book(title, surname, pages, readStatus);
-//     myLibrary.push(book);
-//     bookList.appendChild(li);
-//     form.reset();
-//     listItems = document.querySelectorAll("li");
-//     listItems.forEach((item) => {
-//         item.setAttribute("onclick", "dispInfo()")
-//     });
-// }
+    const book = new Book(title, surname, pages, readStatus);
+    myLibrary.push(book);
+    // bookList.appendChild(li);
+    
+    form.reset();
+    // listItems = document.querySelectorAll("li");
+    // listItems.forEach((item) => {
+    //     item.setAttribute("onclick", "dispInfo()")
+    // });
+}
 
 // function dispInfo() {
 //     alert("It works!");
