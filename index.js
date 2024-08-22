faff34620822c9da378014934b795ad839aabacd
nick <112791837+NCDUBS11@users.noreply.github.com>
2024-08-22 06:19:36 -0700

    Work on moving validation to JS. Implement menu change button

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let form = document.getElementById("form");
+const form = document.getElementById("form");
+const bookListArea = document.getElementById("bookListArea");
+const openBook = document.getElementById("openBookImg");
+const closedBook = document.getElementById("closedBookImg");
+
 let title = document.getElementById("bookTitle");
 let surname = document.getElementById("surname");
 let pages = document.getElementById("pages");
 let readStatus = document.getElementById("readStatus");
-const openBook = document.getElementById("openBookImg");
-const closedBook = document.getElementById("closedBookImg");
 
 const myLibrary = [];
 
@@ -17,6 +19,10 @@ function Book(title, surname, pages, readStatus) {
 }
 
 function addBookToLibrary() {
+
+// errors include regex match checks
+// NAME pattern="[A-Za-z]+{2,}" minlength="2" title="Author surname only. No spaces."
+// PAGES pattern="[1-9]+"
 
     title = title.value.trim();
     surname = surname.value.trim();
@@ -39,6 +45,18 @@ function addBookToLibrary() {
     // });
 }
 
-// function dispInfo() {
-//     alert("It works!");
-// }
+function changeMenu() {
+    if (form.style.display === ""){
+        form.style.display = "none";
+        bookListArea.style.display = "";
+        closedBook.style.display = "none";
+        openBook.style.display = "";
+    }
+
+    else{
+        form.style.display = "";
+        bookListArea.style.display = "none";
+        closedBook.style.display = "";
+        openBook.style.display = "none";
+    }
+}
